294ee84a25230e61f074765a779893b551aff499
marcelo <47941557+marckcm@users.noreply.github.com>
2023-01-10 15:47:56 -0300

    Add files via upload

---
+++ 24_importação_packs.py
@@ -0,0 +1,10 @@
+import vendas.calc_precos as vd
+
+preco = 49.90
+preco_aum = vd.aumento(preco, 15, formata=True)
+print(preco_aum)
+
+preco_red = vd.reducao(preco, 15, formata=True)
+print(preco_red)
+
+print(vd.real(preco))
